d96e8a730855cd8b4bb1d860daed54fe507787f6
Armin Mesic <armin_mesic@hotmail.de>
2019-03-31 17:27:11 +0200

    Create form to submit calculations

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,15 @@ import "phoenix_html"
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        expression: 'Hello Vue!'
+    },
+    methods: {
+        submit: function () {
+            alert(this.expression)
+        }
+    }
+});
